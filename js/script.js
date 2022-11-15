c0b90e375bf6cf9c7647d9eae3b79050e329e6a0
pkarisa <faith.pendo@student.moringaschool.com>
2022-11-16 00:17:24 +0300

    initial commits

---
+++ js/script.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+    jQuery("h1").click(function() {
+        alert("this is a header");      
+    })
+    jQuery("p").click(function(){
+        alert("this is a paragraph");
+    })
+    jQuery("img").click(function(){
+        alert("this is an image")
+    })
+ });
