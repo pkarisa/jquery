25b9db18edde3375841b8aaba64af9d2606b8cae
pkarisa <faith.pendo@student.moringaschool.com>
2022-11-16 10:57:47 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,2 @@
-$(document).ready(function(){
-    $("h1").click(function() {
-        alert("this is a header");      
-    })
-    $("p").click(function(){
-        alert("this is a paragraph");
-    })
-    $("img").click(function(){
-        alert("this is an image")
-    })
- });
+
+
