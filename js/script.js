5c95a05dde0c6d084ca581d9c10438c3fa11ebbb
pkarisa <faith.pendo@student.moringaschool.com>
2022-11-16 23:14:37 +0300

    toggle update

--- js/script.js
+++ js/script.js
@@ -5,8 +5,12 @@ $(document).ready(function(){
  
    
     $("p").click(function() {
+      $(".clickable").click(function(){
+
+        
       $(".walrus-showing").toggle();
       $(".walrus-hidden").toggle();
+      }); 
     });
   });
 
