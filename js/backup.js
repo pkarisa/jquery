723edbc2b85d40e30b543b6b4ebe9a4f690e5a74
pkarisa <faith.pendo@student.moringaschool.com>
2022-12-12 20:05:31 +0300

    object added

--- js/backup.js
+++ js/backup.js
@@ -1,40 +1,7 @@
-$(document).ready(function(){
-    $("p").click(function(){
-    $("img").show();
-       });
-  
-    
-     $("p").click(function() {
-       $(".clickable").click(function(){
- 
-         
-       $(".walrus-showing").toggle();
-       $(".walrus-hidden").toggle();
-       }); 
-     });
-   });
-   $(document).ready(function(){
-    $("#blanks form").submit(function(){
-      $(".person1").append("blah blah");
-      $(".person2").append("blah blah");
-      $(".animal").append("blah blah");
-      $(".Exclamation").append("blah blah");
-      $(".verb").append("blah blah");
-      $(".noun").append("blah blah");
-      $("#story").show();
-    });
-  });
-  $(document).ready(function() {
-    $("#blanks form").submit(function() {
-        $(".person1").append("blah blah");
-        $(".person2").append("blah blah");
-        $(".animal").append("blah blah");
-        $(".exclamation").append("blah blah");
-        $(".verb").append("blah blah");
-        $(".noun").append("blah blah");
-
-        $("#story").show();
-    });
-});
- 
- 
+var moringaStudent = {
+  firstName: "Charlie",
+  lastName: "Obina",
+  level: 1,
+  track: ["Prep","JavaScript","Python", "Django"],
+  enrollmentStatus: true
+};  
---
+++ js/index.js
@@ -0,0 +1,17 @@
+// why we need a server
+// use of node js
+// a system to interact with db
+//system to interacte with files on the machine
+// system to process jobs
+//const externalFile =require('./script')
+//console.log(externalFile)
+//var http = require('http');
+
+var moringaStudent = {
+    firstName: "Charlie",
+    lastName: "Obina",
+    level: 1,
+    track: ["Prep","JavaScript","Python", "Django"],
+    enrollmentStatus: true
+  };  
+
